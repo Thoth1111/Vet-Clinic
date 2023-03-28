4d53ade71f508c561c1c57003c12294729d0ba8c
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-03-28 09:28:07 +0300

    complete table and queries

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals ( 
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100), 
+    date_of_birth date, 
+    escape_attempts INT, 
+    neutered boolean, 
+    weight_kg double precision, 
+    PRIMARY KEY(id) 
+    );
