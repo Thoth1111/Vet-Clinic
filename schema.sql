ccedf34fe38833aaf6d45ca31989acf60d6274a4
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-03-30 14:20:10 +0300

    Add vets and two join tables

--- schema.sql
+++ schema.sql
@@ -38,3 +38,26 @@ REFERENCES species(id);
 ALTER TABLE animals
 ADD COLUMN owner_id INT
 REFERENCES owners(id);
+
+/* Create vets table */
+CREATE TABLE vets (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(100),
+	age INT,
+	date_of_graduation date,
+    PRIMARY KEY(id) 
+);
+
+/* Create specialization join table */
+CREATE TABLE specializations (
+  species_id INT REFERENCES species(id),
+  vets_id INT REFERENCES vets(id),
+  PRIMARY KEY (species_id, vets_id)
+);
+
+/* Create visits join table */
+CREATE TABLE visits (
+  animals_id INT REFERENCES animals(id),
+  vets_id INT REFERENCES vets(id),
+  PRIMARY KEY (animals_id, vets_id)
+);
